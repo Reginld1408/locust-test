6fd0a195cc959401afa4f498dde99cc5099b60f6
Scott <s.irwincowan@gmail.com>
2023-11-04 10:59:17 -0600

    first commit

---
+++ index.py
@@ -0,0 +1,93 @@
+''' Basic Load Testing '''
+
+from flask import Flask
+import time
+
+app = Flask(__name__)
+PORT = 3000
+
+@app.route('/hello')
+def hello():
+    return 'Hello'
+
+@app.route('/slow')
+def slow():
+    time.sleep(2)
+    return 'Slow'
+
+if __name__ == '__main__':
+    app.run(port=PORT)
+
+''' Auth Load Testing '''
+
+# from flask import Flask, request, jsonify
+# import functools
+
+# app = Flask(__name__)
+# PORT = 3000
+
+# def login_middleware(func):
+#     @functools.wraps(func)
+#     def wrapper(*args, **kwargs):
+#         token = request.headers.get('Authorization')
+#         if token != 'secret-token':
+#             return 'Not logged in', 403
+#         return func(*args, **kwargs)
+#     return wrapper
+
+# @app.route('/login', methods=['GET'])
+# def login():
+#     response = {
+#         'token': 'secret-token'
+#     }
+#     return jsonify(response)
+
+# @app.route('/secret', methods=['GET'])
+# @login_middleware
+# def secret():
+#     return 'Welcome to the secret page'
+
+# if __name__ == '__main__':
+#     app.run(port=PORT)
+
+''' Data Load Testing ( Data and Failures ) '''
+
+# from flask import Flask, request, jsonify
+# import functools
+
+# app = Flask(__name__)
+# PORT = 3000
+
+# def login_middleware(func):
+#     @functools.wraps(func)
+#     def wrapper(*args, **kwargs):
+#         token = request.headers.get('Authorization')
+#         if token != 'secret-token':
+#             return 'Not logged in', 403
+#         return func(*args, **kwargs)
+#     return wrapper
+
+# users = [
+#     {"name": "Adam", "id": 0},
+#     {"name": "Bob", "id": 1},
+#     {"name": "Charlie", "id": 2}
+# ]
+
+# @app.route('/login', methods=['GET'])
+# def login():
+#     response = {
+#         'token': 'secret-token'
+#     }
+#     return jsonify(response)
+
+# @app.route('/users/<int:user_id>', methods=['GET'])
+# @login_middleware
+# def get_user(user_id):
+#     user = next((user for user in users if user["id"] == user_id), None)
+#     if user is None:
+#         return 'No user found', 404
+#     return jsonify(user)
+
+# if __name__ == '__main__':
+#     app.run(port=PORT)
+
---
+++ locust.py
@@ -0,0 +1,63 @@
+import time
+import random
+from flask import Flask
+from locust import HttpUser, task, between
+
+''' Basic Load Testing '''
+
+app = Flask(__name__)
+PORT = 3000
+
+
+class MyUser(HttpUser):
+    wait_time = between(1, 5)  # Time between requests in seconds
+    
+    @task
+    def hello(self):
+        self.client.get("/hello")
+
+    @task
+    def slow(self):
+        self.client.get("/slow")
+
+if __name__ == '__main__':
+    app.run(port=PORT)
+    
+    
+''' Auth Load Testing '''
+
+# class QuickstartUser(HttpUser):
+#     def __init__(self, parent):
+#         super(QuickstartUser, self).__init__(parent)
+#         self.token = ""
+
+#     wait_time = between(1, 2)
+
+#     def on_start(self):
+#         with self.client.get(url="/login") as response:
+#             self.token = response.json()["token"]
+
+#     @task
+#     def secret_page(self):
+#         self.client.get(url="/secret", headers={"authorization": self.token})
+
+''' Data Load Testing ( Data and Failures ) '''
+
+# class User(HttpUser):
+#     def __init__(self, parent):
+#         super(User, self).__init__(parent)
+#         self.token = ""
+
+#     wait_time = between(1, 2)
+
+#     def on_start(self):
+#         with self.client.get(url="/login") as response:
+#             self.token = response.json()["token"]
+
+#     @task
+#     def user_page(self):
+#         user_id = str(random.randint(0, 3))
+#         self.client.get(url="/users/" + user_id,
+#                         headers={"authorization": self.token},
+#                         name="Users"
+#                         )
